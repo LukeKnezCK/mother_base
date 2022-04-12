7756f9154178336da58d53cfd9cb9c5e41bff7f4
LukeKnezCK <LukeKnez1386@protonmail.com>
2022-04-11 19:52:21 -0700

    Added some more text boxes and refactored the background stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,46 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../App.css'
-import { Container, Row, Col, Card} from 'react-bootstrap'
+import { Container, Row, Col, Card } from 'react-bootstrap'
+import Support from '../assets/Support.png'
 
 import MBScene from './MBScene'
 
 function Home() {
-    return(
+    return (
         <Container fluid className='outerContainer'>
-            <Row style={{justifyContent:'center'}}>
-                <Col style={{maxWidth:'600px'}}>
-                    <MBScene/>
-                </Col>
-            </Row>
-            <Row style={{justifyContent:'center'}}>
-                <Col style={{maxWidth:'fit-content'}}>
-                    <Card className='textBox'>
-                        <Card.Body className='text1'>WELCOME TO MOTHER BASE</Card.Body>
-                    </Card>
-                </Col>
-            </Row>
+            <Container fluid style={{ maxWidth: '768px' }}>
+                <Row style={{ justifyContent: 'center' }}>
+                    <Col style={{ maxWidth: '600px' }}>
+                        <MBScene />
+                    </Col>
+                </Row>
+                <Row style={{ justifyContent: 'center' }}>
+                    <Col style={{ maxWidth: 'fit-content' }}>
+                        <Card className='textBox'>
+                            <Card.Body className='welcomeSign'>WELCOME TO MOTHER BASE</Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col style={{ maxWidth: 'fit-content', marginLeft: '5%' }}>
+                        <Card className='labelBox'>
+                            <Card.Body className='labelCard'><img className='icon' src={Support} alt='Support Icon' /> About</Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col> 
+                        <Card className='textBox'>
+                            <Card.Body className='longText'> Here at Mother Base we are commited to the research and
+                                development of technologies that are useful for both
+                                arbirtrary niche personal projects as well as increasing the
+                                chances of obtaining gainful employment in order to avoid
+                                the possibility of becoming completely impoverished
+                                and destitute, living in refuse and squalor.</Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </Container>
     )
 }
